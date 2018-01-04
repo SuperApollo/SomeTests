13b0118bff6bfbd531898e0bebcf40e590da9b5e
APOLLO\Administrator <wpb504@126.com>
2018-01-04 17:48:01 +0800

    测试

--- TestVerticalViewPager/app/src/main/java/com/example/administrator/testverticalviewpager/SecondActivity.java
+++ TestVerticalViewPager/app/src/main/java/com/example/administrator/testverticalviewpager/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.administrator.testverticalviewpager;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -10,6 +12,8 @@ import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
+import com.example.administrator.testverticalviewpager.widget.RingProgressBar;
+
 /**
  * Created by Administrator on 2017/12/12.
  */
@@ -17,16 +21,25 @@ import android.widget.ImageView;
 public class SecondActivity extends Activity {
 
     private ImageView mIvReadyGo;
+    private static RingProgressBar mBar1;
+    private static RingProgressBar mBar2;
+    private static final int UPDATE_PROGRESS = 0;
+    private static MyHandler mHandler;
+    private static int progress = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         mIvReadyGo = findViewById(R.id.iv_ready_go);
+        mBar1 = findViewById(R.id.progress_bar_1);
+        mBar2 = findViewById(R.id.progress_bar_2);
+        mHandler = new MyHandler();
         findViewById(R.id.btn_ready_go).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 readyAnim();
+                mHandler.sendEmptyMessageDelayed(UPDATE_PROGRESS,100);
             }
         });
     }
@@ -91,6 +104,22 @@ public class SecondActivity extends Activity {
 
         mIvReadyGo.setVisibility(View.VISIBLE);
         mIvReadyGo.startAnimation(set);
+    }
+
+    private static class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_PROGRESS:
+                    if (progress < 100) {
+                        progress++;
+                        mBar1.setProgress(progress);
+                        mBar2.setProgress(progress);
+                        mHandler.sendEmptyMessageDelayed(UPDATE_PROGRESS, 100);
+                        break;
+                    }
+            }
+        }
     }
 
 }
